bd14132e88188f69000634062a34beed7c9b68d7
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-01-08 12:37:46 +0600

    vercel change 3

--- server/index.js
+++ server/index.js
@@ -6,22 +6,15 @@ import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 
 // env connect
 dotenv.config();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const username = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
 
 // app create
 const app = express();
 
-const corsConfig = {
-	origin: "",
-	credentials: true,
-	methods: ["GET", "POST", "PUT", "DELETE"],
-};
 // middlewares
-app.use([cors(corsConfig), morgan("dev"), express.json()]);
-app.options("", cors(corsConfig));
-
+app.use([cors(), morgan("dev"), express.json()]);
 
 // app health check
 app.get("/", (req, res) => {
